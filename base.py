b6d5fcb8fafc22596556be2e6e23cb81bb70158a
Nikolaj Starodubtsev <nstarodubtsev@mirantis.com>
2012-03-06 10:45:52 +0400

    base ed.0 unworke

--- base.py
+++ base.py
@@ -1,22 +1,22 @@
 import MySQLdb as mdb
 import sys
 import server
-import connection as xk
-con = mdb.connect(read_default_file='./.conf.cnf');
+
+xk = mdb.connect(read_default_file='./.conf.cnf');
 
 
 def insert(msg):
     with con:
-        cur = con.cursor()
+        cur = xk.cursor()
         cur.execute("CREATE TABLE IF NOT EXISTS\
-                     Jobs(Id INT PRIMARY KEY AUTO_INCREMENT,
-                          Task VARCHAR(25),
+                     Jobs(Id INT PRIMARY KEY AUTO_INCREMENT,\
+                          Task VARCHAR(25),\
                           Time TIMESTAMP)")
         cur.execute("INSERT INTO Jobs(Task) VALUES(msg)")
 
 
 def outg():
-    with con:
-        cur = con.cursor()
+    with xk:
+        cur = xk.cursor()
         ki = int(cur.execute("SELECT LAST_INSERT_ID()"))
     return (ki)
