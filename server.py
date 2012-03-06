cdf8ccc2520fe77a4f2868f2e9573a7a7183096c
Nikolaj Starodubtsev <nstarodubtsev@mirantis.com>
2012-03-06 10:41:43 +0400

    nobasemod

--- server.py
+++ server.py
@@ -1,5 +1,27 @@
-import pika 
-import base
+import pika
+import _mysql as mdb
+import sys
+
+
+xk = mdb.connect(read_default_file='conf.cnf');
+
+
+def insert(msg):
+    with xk:
+        cur = xk.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS\
+                     Jobs(Id INT PRIMARY KEY AUTO_INCREMENT,\
+                     Task VARCHAR(25),\
+                     Time TIMESTAMP)")
+        cur.execute("INSERT INTO Jobs(Task) VALUES(msg)")
+
+
+def outg():
+    with con:
+        cur = xk.cursor()
+        ki = int(cur.execute("SELECT LAST_INSERT_ID()"))
+    return (ki)
+
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
@@ -10,8 +32,8 @@ channel.queue_declare(queue='rpc_queue')
 
 
 def on_request(ch, method, props, body):
-    #base.insert(body)
-    response = base.outg(n)
+    insert(body)
+    response = outg(n)
     ch.basic_publish(exchange='',
                      routing_key=props.reply_to,
                      properties=pika.BasicProperties(correlation_id=\
