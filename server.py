2db40b279ecea2788b873a5bcb2924b92524e3bd
Nikolaj Starodubtsev <nstarodubtsev@mirantis.com>
2012-03-12 11:33:34 +0400

    works with errors

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import sys
 
 
 def cut (x):
-    x = x.strip("\n")
+    x = x.strip("\n()L,")
     return(x)
 
 
@@ -21,24 +21,28 @@ xk = mdb.connect(a, b, c, d)
 
 
 def insert(msg):
+    t = str(msg)
     with xk:
         cur = xk.cursor()
         cur.execute("CREATE TABLE IF NOT EXISTS\
                      Jobs(Id INT PRIMARY KEY AUTO_INCREMENT,\
                      Task VARCHAR(25),\
                      Time TIMESTAMP)")
-        cur.execute("INSERT INTO Jobs(Id, Task, Time) VALUES(%s,msg,%s)")
+        cur.execute("INSERT INTO Jobs(Task) VALUES('%s')" % \
+                      (t))
 
 
 def outg():
-    with con:
+    with xk:
         cur = xk.cursor()
-        ki = int(cur.execute("SELECT LAST_INSERT_ID()"))
+        cur.execute("SELECT Id FROM Jobs")
+        res = cur.fetchall()
+        ki = str(res[-1])
     return (ki)
 
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
+        host=a))
 
 channel = connection.channel()
 
@@ -47,7 +51,7 @@ channel.queue_declare(queue='rpc_queue')
 
 def on_request(ch, method, props, body):
     insert(body)
-    response = outg(n)
+    response = cut(outg())
     ch.basic_publish(exchange='',
                      routing_key=props.reply_to,
                      properties=pika.BasicProperties(correlation_id=\
