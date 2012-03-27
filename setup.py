ce0acbff47bd449d2fb3da9052b0582a498afe7a
Nikolaj Starodubtsev <nstarodubtsev@mirantis.com>
2012-03-27 10:33:14 +0400

    Setup script is here

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='0_prod',
+      version='0.9',
+      description='Client server app worked thru RabbitMQ server',
+      author='Nikolaj Starodubtsev',
+      author_email='starodubcevna@gmail.com',
+      py_modules=['server', 'client', 'createbase'],
+      data_files=[('conf',['conf.cnf'])]
+     )
