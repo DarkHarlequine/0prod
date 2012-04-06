38528cdf15c4852c1e7011f8efb088ba151e71ae
Nikolaj Starodubtsev <nstarodubtsev@mirantis.com>
2012-04-06 10:16:05 +0400

    Update setup.py. Commands setup.py pep8 and setup.py pyflakes are now available

--- setup.py
+++ setup.py
@@ -1,37 +1,38 @@
 from distutils.core import setup, Command
-class Pep8py(Command):
+import commands
+class Pep8(Command):
     user_options = []
     def initialize_options(self):
         pass
     def finalize_options(self):
         pass
     def run(self):
-        import sys,subprocess
-        errno = subprocess.call([sys.executable,\
-                                '/usr/lib/python2.7/dist-packages/pep8.py',\
-                                raw_input("Please input module name \n")])
-        raise SystemExit(errno)
-class Pyflakespy(Command):
+        module = raw_input ("Please insert module name\n")       
+        rez = commands.getstatusoutput('pep8 %s' % (module))
+        if rez == (0, ''):
+            rez = 'No errors'
+        raise SystemExit(rez)
+class Pyflakes(Command):
     user_options = []
     def initialize_options(self):
         pass
     def finalize_options(self):
         pass
     def run(self):
-        import sys,subprocess
-        errno = subprocess.call([sys.executable,\
-        '/usr/local/lib/python2.7/dist-packages/pyflakes/scripts/pyflakes.py',\
-                                raw_input("Please input module name \n")])
-        raise SystemExit(errno)
+        module = raw_input ("Please insert module name\n")       
+        rez = commands.getstatusoutput('pyflakes %s' % (module))
+        if rez == (0, ''):
+            rez = 'No errors'
+        raise SystemExit(rez)
 
 setup(name='0_prod',
       version='0.9',
       description='Client server app worked thru RabbitMQ server',
       author='Nikolaj Starodubtsev',
       author_email='starodubcevna@gmail.com',
-      py_modules=['server', 'client', 'createbase'],
+      py_module=['server', 'client', 'createbase'],
       data_files=['./etc/conf.cnf', 'Readme'],
       scripts=['setconnect.py'],
-      cmdclass={'pep8':Pep8py,
-                'pyflakes':Pyflakespy}
+      cmdclass={'pep8':Pep8,
+                'pyflakes':Pyflakes}
      )
